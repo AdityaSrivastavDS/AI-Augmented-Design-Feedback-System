56e472f64195037bed3735ad430ed2fcb2aaf2f6
ADITYA SRIVASTAV <adityathestar2006@gmail.com>
2024-08-25 11:25:01 -0700

    commit updates

--- Dataset Generation and Model Training/Model Training/train_model.py
+++ Dataset Generation and Model Training/Model Training/train_model.py
@@ -5,9 +5,9 @@ from sklearn.ensemble import RandomForestRegressor
 import joblib
 
 # Load datasets
-performance_data = pd.read_csv('E:/Aditya/Projects/AI_Augmented_Design_Feedback_System/models/performance_data.csv')
-safety_data = pd.read_csv('E:/Aditya/Projects/AI_Augmented_Design_Feedback_System/models/safety_data.csv')
-efficiency_data = pd.read_csv('E:/Aditya/Projects/AI_Augmented_Design_Feedback_System/models/efficiency_data.csv')
+performance_data = pd.read_csv('E:\\Aditya\\Projects\\AI_Augmented_Design_Feedback_System\\Dataset Generation and Model Training\\Model Training\\performance_data.csv')
+safety_data = pd.read_csv('E:\\Aditya\\Projects\\AI_Augmented_Design_Feedback_System\\Dataset Generation and Model Training\\Model Training\\safety_data.csv')
+efficiency_data = pd.read_csv('E:\\Aditya\\Projects\\AI_Augmented_Design_Feedback_System\\Dataset Generation and Model Training\\Model Training\\efficiency_data.csv')
 
 # Performance model
 X_performance = performance_data.drop('performance_score', axis=1)
@@ -28,7 +28,7 @@ efficiency_model = RandomForestRegressor()
 efficiency_model.fit(X_efficiency, y_efficiency)
 
 # Define the path where you want to save the models
-model_save_path = 'E:/Aditya/Projects/AI_Augmented_Design_Feedback_System/models'
+model_save_path = 'E:\\Aditya\\Projects\\AI_Augmented_Design_Feedback_System\\models'
 
 # Save models to disk
 joblib.dump(performance_model, model_save_path + 'performance_model.pkl')
@@ -36,3 +36,4 @@ joblib.dump(safety_model, model_save_path + 'safety_model.pkl')
 joblib.dump(efficiency_model, model_save_path + 'efficiency_model.pkl')
 
 print(f"Models trained and saved to {model_save_path}")
+
